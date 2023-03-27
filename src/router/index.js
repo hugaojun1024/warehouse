e8ccfe80e74acc1ccf5fc2b440f0736df1b602e4
Abu <hugaojun1024@163.com>
2023-03-27 19:35:07 +0800

    commit "FrontEnd -Log In"

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import LoginUser from '../components/LoginUser.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: LoginUser
   },
   {
     path: '/about',
@@ -17,6 +18,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/loginUser',
+    component: LoginUser
   }
 ]
 
