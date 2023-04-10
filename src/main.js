495f3e4bb38d1dc674840c5a7f65f56a53061c1a
zhangds <zhangds01@outlook.com>
2023-04-10 19:13:06 +0800

    test

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+//
 new Vue({
   router,
   render: h => h(App)
