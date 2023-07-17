16ee31dddf01bd638ec060f562cd6757a8791ddd
Mr阿布 <hugaojun1024@163.com>
2023-07-17 16:02:14 +0800

    添加--阿里云远程访问端口

--- utils/request.js
+++ utils/request.js
@@ -4,6 +4,7 @@ import router from "@/router";
 
 const request = axios.create({
   baseURL: 'http://localhost:8081',  // 注意！！ 这里是全局统一加上了 '/api' 前缀，也就是说所有接口都会加上'/api'前缀在，页面里面写接口的时候就不要加 '/api'了，否则会出现2个'/api'，类似 '/api/api/user'这样的报错，切记！！！
+  // baseURL: 'http://47.113.180.76:8081',  //阿里云远程
   timeout: 30000
 })
 
@@ -27,8 +28,8 @@ request.interceptors.response.use(
   (response) => response,
   ({ response }) => {
     const {status, data} = response;
-    // const {message} = data;
-    // console.log("message Response ：" + message)
+    const {message} = data;
+    console.log("message Response ：" + message)
     console.log(status)
     if (status === 401) {
       Message.error("请先登录以获得访问权限");
